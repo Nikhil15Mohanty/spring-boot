edf42e50fc387cb82ec171ea50ce872e460fd121
Nikhil Mohanty <nikhilviki.15@gmail.com>
2024-02-20 23:21:26 +0530

    @Autowired concept is used

---
+++ src/main/java/com/example/Component/demoComponent/Autowired/VegPizza.java
@@ -0,0 +1,11 @@
+package com.example.Component.demoComponent.Autowired;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class VegPizza {
+    public String type() {
+        return "Veg pizza type";
+    }
+
+}
--- src/main/java/com/example/Component/demoComponent/Controller/PizzaController.java
+++ src/main/java/com/example/Component/demoComponent/Controller/PizzaController.java
@@ -1,10 +1,23 @@
 package com.example.Component.demoComponent.Controller;
 
+import com.example.Component.demoComponent.Autowired.VegPizza;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PizzaController {
+
+    private VegPizza v;
+  /*  @Autowired
+    PizzaController(VegPizza v){
+        this.v = v;
+    }*/
+    @Autowired
+   public void setPizza(VegPizza v){
+        this.v = v;
+    }
+
     public String display(){
-        return "Pizza is delicious";
+        return v.type();
     }
 }
